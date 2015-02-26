759ba807910412779ee879827e2ef3f9838fcd28
Lloyd Cledwyn <cledwyn@gmail.com>
2015-02-25 19:30:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
+<?php
+	$hostname_db = "mysql.seis752.com";
+	$database_db = "seis752";
+	$username_db = "seis752student";
+	$password_db = "gradsoftware";
+?>
 <html>
   <body>
-    <h1>Hi Class - PHP</h1>
+    <ol>
+    <?php
+		$con = mysql_connect($hostname_db,$username_db,$password_db); 
+		if (!$con)
+		  {
+		  die('Could not connect: ' . mysql_error());
+		  }
+		
+		mysql_select_db($database_db, $con);
+		
+		$result = mysql_query("SELECT * FROM users where name like '%$name%' limit 20");
+		
+		while($row = mysql_fetch_array($result))
+		  {
+		  echo '<li>'.$row['name'] . " - " . $row['username'];
+		  echo "</li>";
+		  }
+		
+		mysql_close($con);
+	?>
+    </ol>
   </body>
 </html>
